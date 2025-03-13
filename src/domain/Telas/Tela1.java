29018b11437d97f160f1ee9f1defb7364754cc33
Administrador <p.pecoo78@gmail.com>
2025-03-13 12:49:45 -0300

    Feature com 2 tela funcional fix identation

--- src/domain/Telas/Tela1.java
+++ src/domain/Telas/Tela1.java
@@ -5,7 +5,6 @@ import dao.IClienteDAO;
 import domain.Cliente;
 
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -18,57 +17,58 @@ public class Tela1 extends JFrame {
     private JButton SairButton;
     private JButton pesquisarButton;
     private Cliente cliente;
-
+    
     public Tela1() {
         Cliente Test = new Cliente("Peter Parker", "Rua Brasil", 1, "Minas Gerais", "Belo Horizonte", 313482, 521);
         getClienteDAO().cadastrar(Test);
-
+        
         setContentPane(MainPanel);
         setTitle("Menu Principal");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500, 300);
         setLocationRelativeTo(null);
         setVisible(true);
-
+        
         SairButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-            actionSair();
-
+                actionSair();
+                
+                
             }
         });
-
+        
         cadastrarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                
                 actionCadastrarUsuario();
             }
         });
-
+        
         mostrarTodosButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-
+                
+                
                 IClienteDAO clienteDAO = getClienteDAO();
                 StringBuilder todosClientes = new StringBuilder();
-
+                
                 for (Cliente cliente : clienteDAO.buscarTodos()) {
                     todosClientes.append("Nome: ").append(cliente.getNome())
                             .append(", CPF: ").append(cliente.getCpf())
                             .append("\n");
                 }
-
+                
                 if (todosClientes.length() > 0) {
                     JOptionPane.showMessageDialog(null, todosClientes.toString(), "Todos os Clientes", JOptionPane.INFORMATION_MESSAGE);
                 } else {
                     JOptionPane.showMessageDialog(null, "Nenhum cliente encontrado.", "Todos os Clientes", JOptionPane.INFORMATION_MESSAGE);
                 }
-
+                
             }
         });
-
+        
         excluirButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -79,21 +79,21 @@ public class Tela1 extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 actionBuscarCPFUnico();
-
-                    }
-
-
+                
+            }
+            
+            
         });
     }
-
+    
     private void actionDeletar() {
-
+        
         try {
 //          JFormattedTextField formattedTextField = new JFormattedTextField(new javax.swing.text.MaskFormatter("###.###.###-##"));
             JFormattedTextField formattedTextField = new JFormattedTextField();
-
+            
             formattedTextField.setColumns(11); // Tamanho do campo
-
+            
             int option = JOptionPane.showConfirmDialog(
                     null,
                     formattedTextField,
@@ -101,40 +101,38 @@ public class Tela1 extends JFrame {
                     JOptionPane.OK_CANCEL_OPTION,
                     JOptionPane.PLAIN_MESSAGE
             );
-
+            
             if (option == JOptionPane.OK_OPTION) {
 //                String cpfString = formattedTextField.getText().replaceAll("[^0-9]", ""); // Isso tira os traços
                 String cpfString = formattedTextField.getText();
                 Integer cpf = Integer.valueOf(cpfString);
-
+                
                 IClienteDAO clienteDAO = getClienteDAO();
                 Cliente clienteRemovido = clienteDAO.excluir(cpf);
-
+                
                 if (clienteRemovido != null) {
                     JOptionPane.showMessageDialog(null, "Cliente removido com sucesso: " + clienteRemovido.getNome(), "Sucesso", JOptionPane.INFORMATION_MESSAGE);
                 } else {
                     JOptionPane.showMessageDialog(null, "Nenhum cliente encontrado com o CPF: " + cpf, "Erro", JOptionPane.ERROR_MESSAGE);
                 }
             }
-
+            
         } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, "CPF inválido! Por favor, digite apenas números.", "Erro", JOptionPane.ERROR_MESSAGE);
         }
-
+        
     }
-
-    public void actionCadastrarUsuario()
-    {
-            TelaCadastro telaCadastro = new TelaCadastro();
+    
+    public void actionCadastrarUsuario() {
+        TelaCadastro telaCadastro = new TelaCadastro();
     }
-
-    public void actionBuscarCPFUnico()
-    {
-
+    
+    public void actionBuscarCPFUnico() {
+        
         JFormattedTextField formattedTextField = new JFormattedTextField();
         IClienteDAO clienteDAO = getClienteDAO();
         formattedTextField.setColumns(15);
-
+        
         JPanel panel = new JPanel();
         panel.add(new JLabel("Digite o CPF a ser buscado: "));
         panel.add(formattedTextField);
@@ -143,13 +141,13 @@ public class Tela1 extends JFrame {
                 panel,
                 "Entrada de CPF",
                 JOptionPane.PLAIN_MESSAGE);
-
+        
         String cpfString = formattedTextField.getText();
-
+        
         try {
             Integer cpf = Integer.valueOf(cpfString);
             cliente = getClienteDAO().consultar(cpf);
-
+            
             if (cliente != null) {
                 clienteDAO.consultar(cpf);
                 JOptionPane.showMessageDialog(null, "Usuário encontrado: " + cliente.getNome(), "Sucesso", JOptionPane.INFORMATION_MESSAGE);
@@ -159,21 +157,17 @@ public class Tela1 extends JFrame {
         } catch (NumberFormatException o) {
             JOptionPane.showMessageDialog(null, "Digite um CPF válido! Apenas números são permitidos.", "Erro", JOptionPane.ERROR_MESSAGE);
         }
-
-
-
-
-
+        
+        
     }
-
-    public void actionSair()
-    {
+    
+    public void actionSair() {
         JOptionPane.showMessageDialog(null, "Até Logo!!", "Mensagem", JOptionPane.INFORMATION_MESSAGE);
         System.exit(0);
     }
-
+    
     private IClienteDAO getClienteDAO() {
         return ClienteMapDAO.getInstance();
     }
-
+    
 }
